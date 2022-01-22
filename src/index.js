45fedf19a11a4062ac57f4bc5e6c09b08e6f16ff
hoangvunguyen01 <hoangvunguyen01@gmail.com>
2022-01-22 21:53:46 +0700

    Add boostrap

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.use(morgan('combined'))
+
+const exphbs = require('express-handlebars')
+const hbs = exphbs.create({ extname: '.hbs' })
+// TEMPLATE ENGINE
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resources/views'));
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/news', (req, res) => {
+    res.render('news')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
